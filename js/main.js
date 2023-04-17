e0f234116eaebfac706101efd882b89d3c4e431c
chawonlee <ss01066865089@gmail.com>
2023-04-17 21:30:22 +0900

    1차수정

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.mainSwiper', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
