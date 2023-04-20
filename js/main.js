8bd990bd00ffb5a655fbf2f6534f21cd79b0e090
chawonlee <ss01066865089@gmail.com>
2023-04-20 20:52:42 +0900

    sub swiper

--- js/main.js
+++ js/main.js
@@ -15,3 +15,21 @@ const swiper = new Swiper('.mainSwiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+const subSwiper = new Swiper ('.subSwiper', {
+  autoplay: { // 자동 재생 여부
+    delay: 5000 // 5초마다 슬라이드 바뀜
+  },
+  loop: true, // 반복 재생 여부
+  slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  pagination: { // 페이지 번호 사용 여부
+    el: '.subSwiper .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+    prevEl: '.subSwiper .swiper-prev', // 이전 버튼 선택자
+    nextEl: '.subSwiper .swiper-next' // 다음 버튼 선택자
+  }
+})
